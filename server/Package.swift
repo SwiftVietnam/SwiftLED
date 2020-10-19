6520efc289edaadef966b152e8f64b4504a1014b
An Tran <tran.binhan@gmail.com>
2020-10-19 23:06:35 +0800

    Add server

---
+++ server/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLED",
+    dependencies: [
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.1.10"),
+    .package(url: "https://github.com/uraimo/WS281x.swift.git", from: "2.0.4")
+    ],
+    targets: [
+        .target(
+            name: "SwiftLED",
+            dependencies: [
+                "SwiftyGPIO",
+                "WS281x"
+            ]
+        )
+    ]
+)
---
+++ server/Sources/SwiftLED/main.swift
@@ -0,0 +1,24 @@
+import SwiftyGPIO
+import WS281x
+import Glibc
+import Foundation
+
+let pwms = SwiftyGPIO.hardwarePWMs(for: .RaspberryPiPlusZero)!
+let pwm = (pwms[0]?[.P18])!
+
+let numberOfLeds = 8
+
+let w = WS281x(
+    pwm, 
+    type: .WS2812B,
+    numElements: numberOfLeds
+)
+
+//var color: UInt32 = 0xFFFFFF
+var color: UInt32 = 0x0
+
+w.setLeds([UInt32](repeating: color, count: numberOfLeds))
+
+w.start()
+w.wait()
+
