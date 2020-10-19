5e98145695d1b642d93867132abc6a91f24bb7a9
An Tran <tran.binhan@gmail.com>
2020-10-19 23:45:07 +0800

    Update Package

--- server/Package.swift
+++ server/Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftLED",
     dependencies: [
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.1.10"),
-    .package(url: "https://github.com/uraimo/WS281x.swift.git", from: "2.0.4")
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.3.1"),
+        .package(url: "https://github.com/uraimo/WS281x.swift.git", from: "2.0.4"),
     ],
     targets: [
         .target(
