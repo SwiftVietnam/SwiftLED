993e1006d7a74a45bb4c7889dec762adfaf7a4ff
An Tran <tran.binhan@gmail.com>
2020-10-21 22:27:38 +0800

    Add UI

--- server/Package.swift
+++ server/Package.swift
@@ -7,10 +7,12 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
         .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.3.1"),
         .package(url: "https://github.com/uraimo/WS281x.swift.git", from: "2.0.4"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: [
             "Vapor",
+            "Leaf",
             "SwiftyGPIO",
             "WS281x"            
         ]),
